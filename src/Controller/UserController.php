e609d9ff9740840e0eb8ac1fcd049fecff1b4f0c
Hugo LIEGEARD <hugo@hl-media.co>
2023-10-11 16:48:08 +0200

    Mise en place de la BDD avec Doctrine

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/login')]
+    public function login(): Response
+    {
+        return $this->render('user/login.html.twig');
+    }
+}
