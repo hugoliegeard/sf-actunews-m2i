5e386af0f09ba6eaf6fd0efbcfb12c9c4e54ffca
Hugo LIEGEARD <hugo@hl-media.co>
2023-10-11 11:44:41 +0200

    Mise en place theme HTML

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     public function home(): Response
     {
-        return new Response("<h1>PAGE ACCUEIL</h1>");
+        return $this->render('default/home.html.twig');
     }
 
     /**
@@ -22,7 +23,7 @@ class DefaultController
     #[Route('/{slug<\w+>}', name: 'default_category', methods: ['GET'])]
     public function category(string $slug): Response
     {
-        return new Response("<h1>PAGE CATEGORY : $slug</h1>");
+        return $this->render('default/category.html.twig');
     }
 
     /**
@@ -36,6 +37,6 @@ class DefaultController
     #[Route('/{categorySlug<\w+>}/{slug}_{id<\d+>}.html', name: 'default_post', methods: ['GET'])]
     public function post(string $categorySlug, string $slug, int $id): Response
     {
-        return new Response("<h1>PAGE ARTICLE : $categorySlug \ $slug - $id</h1>");
+        return $this->render('default/post.html.twig');
     }
 }
